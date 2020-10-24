5ba9d06ef50d038014d0d670ead1c61b71016e2d
Юлия Шипунова <yu.shipunowa@yandex.ru>
2020-10-25 01:18:20 +0300

    Laba 2 is ready

--- WindowsFormsApp1/WindowsFormsApp1/Plane.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Plane.cs
@@ -2,37 +2,39 @@
 
 namespace Laboratornaya
 {
-    class Plane
+    class Plane : IAdditions
     {
-        private PlaneEnum planeEnum;
-       
-        public int Digit { set => planeEnum = (PlaneEnum)value; }
-       
-        public void Draw(Graphics g, float _startPosX, float _startPosY)
-        {
-            switch (planeEnum)
-            {
-                case PlaneEnum.Three:
-                    DrawPlaneThree(g, _startPosX, _startPosY);
-                    break;
-
-                case PlaneEnum.Four:
-                    DrawPlaneFour(g, _startPosX, _startPosY);
-                    break;
-
-                case PlaneEnum.Five:
-                    DrawPlaneFive(g, _startPosX, _startPosY);
-                    break;
-            }
-        }
+        private PlanesEnum planeEnum;
 
         public Plane(int digit)
         {
             Digit = digit;
         }
 
-        void DrawPlaneThree(Graphics g, float _startPosX, float _startPosY)
+        public int Digit { set => planeEnum = (PlanesEnum)value; }
+
+        public void DrawAdditions(Graphics g, float _startPosX, float _startPosY)
         {
+            switch (planeEnum)
+            {
+                case PlanesEnum.Three:
+                    DrawPlaneThree(g, _startPosX, _startPosY);
+                    break;
+                case PlanesEnum.Four:
+                    DrawPlaneFour(g, _startPosX, _startPosY);
+                    break;
+                case PlanesEnum.Five:
+                    DrawPlaneFive(g, _startPosX, _startPosY);
+                    break;
+            }
+        }
+
+        private void DrawPlaneThree(Graphics g, float _startPosX, float _startPosY)
+        {
+            g.DrawLine(new Pen(Color.Black), _startPosX + 104, _startPosY + 27, _startPosX + 104, _startPosY + 43);
+            g.DrawLine(new Pen(Color.Black), _startPosX + 104, _startPosY + 35, _startPosX + 120, _startPosY + 35);
+            g.FillRectangle(new SolidBrush(Color.LightSlateGray), _startPosX + 111, _startPosY + 26, 5, 20);
+
             g.DrawLine(new Pen(Color.Black), _startPosX + 40, _startPosY + 27, _startPosX + 40, _startPosY + 43);
             g.DrawLine(new Pen(Color.Black), _startPosX + 40, _startPosY + 35, _startPosX + 56, _startPosY + 35);
             g.FillRectangle(new SolidBrush(Color.LightSlateGray), _startPosX + 47, _startPosY + 26, 5, 20);
@@ -42,19 +44,17 @@ namespace Laboratornaya
             g.FillRectangle(new SolidBrush(Color.LightSlateGray), _startPosX + 63, _startPosY + 49, 5, 18);
         }
 
-        void DrawPlaneFour(Graphics g, float _startPosX, float _startPosY)
+        private void DrawPlaneFour(Graphics g, float _startPosX, float _startPosY)
         {
             DrawPlaneThree(g, _startPosX, _startPosY);
-
             g.DrawLine(new Pen(Color.Black), _startPosX + 72, _startPosY + 27, _startPosX + 72, _startPosY + 43);
             g.DrawLine(new Pen(Color.Black), _startPosX + 72, _startPosY + 35, _startPosX + 88, _startPosY + 35);
             g.FillRectangle(new SolidBrush(Color.LightSlateGray), _startPosX + 79, _startPosY + 26, 5, 20);
         }
 
-        void DrawPlaneFive(Graphics g, float _startPosX, float _startPosY)
+        private void DrawPlaneFive(Graphics g, float _startPosX, float _startPosY)
         {
             DrawPlaneFour(g, _startPosX, _startPosY);
-
             g.DrawLine(new Pen(Color.Black), _startPosX + 88, _startPosY + 48, _startPosX + 88, _startPosY + 64);
             g.DrawLine(new Pen(Color.Black), _startPosX + 88, _startPosY + 56, _startPosX + 104, _startPosY + 56);
             g.FillRectangle(new SolidBrush(Color.LightSlateGray), _startPosX + 95, _startPosY + 49, 5, 18);
---
+++ WindowsFormsApp1/WindowsFormsApp1/Ship.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace Laboratornaya
+{
+    public abstract class Ship : IWaterTransport
+    {
+        //левая координата отрисовки корабля
+        protected float _startPosX;
+
+        //правая координата отрисовки корабля
+        protected float _startPosY;
+
+        //ширина окна отрисовки
+        protected int _pictureWidth;
+
+        //высота окна отрисовки
+        protected int _pictureHeight;
+
+        // максимальная скорость
+        public int MaxSpeed { protected set; get; }
+
+        // вес
+        public float Weight { protected set; get; }
+
+        // главный цвет
+        public Color MainColor { protected set; get; }
+
+        //установка позиции авианосца
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            if ((_startPosX >= 0 && _startPosX < _pictureWidth) &&
+                (_startPosY >= 0 && _startPosY < _pictureHeight))
+            {
+                _startPosX = x;
+                _startPosY = y;
+            }
+            _pictureHeight = height;
+            _pictureWidth = width;
+        }
+
+        public abstract void DrawWaterTransport(Graphics g);
+
+        public abstract void MoveTransport(Direction direction);
+    }
+}
